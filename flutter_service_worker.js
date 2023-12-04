93d9426f525d4f2e2021b12f389a785c3cee5d16
m0stafa108 <149520976+m0stafa108@users.noreply.github.com>
2023-12-05 01:08:20 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,8 +23,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "ca180f689b098f053fc0bbf2885953de",
 "icons/Icon-maskable-192.png": "d0687b175d029ca80d03d071a244678f",
 "icons/Icon-maskable-512.png": "ca180f689b098f053fc0bbf2885953de",
-"index.html": "914a999b1be76836032e688d71b228af",
-"/": "914a999b1be76836032e688d71b228af",
+"index.html": "c9279f86468785554afc336b2d7bb24d",
+"/": "c9279f86468785554afc336b2d7bb24d",
 "main.dart.js": "b6ca9e8108f1f1e01b256a01f71c685d",
 "manifest.json": "35f904f76d11eed3ae2b03556d202c0a",
 "version.json": "9469cb0652e13641c3e8931a36f3e3da"};
